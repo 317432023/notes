11e246d5a4ad3ebee7f4672780e380239b20fce0
kangtengjiao <317432023@qq.com>
2024-03-06 15:29:56 +0800

    idea学习笔记

--- MySQL/mysql5.7/注册mysql5.7服务.cmd
+++ MySQL/mysql5.7/注册mysql5.7服务.cmd
@@ -2,7 +2,7 @@ REM 请以管理员身份执行此批处理文件
 set mysql_home=%~dp0
 cd /d %mysql_home%bin
 REM 生成data目录 首次安装不要自己创建
-REM mysqld --initialize-insecure --console
+REM mysqld --initialize--insecure --console
 
 mysqld --install mysql5.7
 
